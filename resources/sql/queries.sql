0e078689d99b70fb514da130371928a1ee4e52b1
Phil Hunt <phil@deviantlogic.co.uk>
2021-05-09 17:36:34 +0100

    Regression tests on guestbook.db

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,6 +1,7 @@
 -- :name save-message! :! :n
 -- :doc creates a new message using name and msg keys
-INSERT INTO guestbook (name, message)
+INSERT INTO guestbook
+(name, message)
 VALUES (:name, :message)
 -- :name get-messages :? :*
 -- :doc selects all available messages
