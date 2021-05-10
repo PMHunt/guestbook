0aa90323515740577176a2cd6b1651ea85b9581c
Phil Hunt <phil@deviantlogic.co.uk>
2021-05-10 19:31:09 +0100

    Messages and post messages, but screens.css not loading??

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -8,15 +8,21 @@
    [ring.util.http-response :as response]))
 
 (defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+  (layout/render request "home.html" {:messages (db/get-messages)}))
 
 (defn about-page [request]
   (layout/render request "about.html"))
+
+(defn save-message! [{:keys [params]}]
+  ;; destructure request by key into params and save to db
+  (db/save-message! params)
+  ;; redirect back to homepage
+  (response/found "/"))
 
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/about" {:get about-page}]])
-
+   ["/about" {:get about-page}]
+   ["/message" {:post save-message!}]])
